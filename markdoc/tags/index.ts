05c4c0fd218fe483e12f3e57109c106fdd614756
Julian Willox <julianwillox@Julians-MacBook-Pro.local>
2022-11-01 11:20:22 +0200

    fix: added ts no check

--- markdoc/tags/index.ts
+++ markdoc/tags/index.ts
@@ -1,4 +1,5 @@
 /* Use this file to export your markdoc tags */
 export * from './callout.markdoc';
 export * from './listItem.markdoc';
+//@ts-ignore
 export { comment, head, link, script } from '@markdoc/next.js/tags';
