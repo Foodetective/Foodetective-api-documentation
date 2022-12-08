57526747bca64620f68f58f9fd69829d65210e49
Josh Willox <joshwillox@gmail.com>
2022-12-08 22:45:55 +1100

    feat: add husky and lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  "*.{js,jsx,ts,tsx}": [
+    (filenames) =>
+      `next lint --max-warnings=0 --fix --file ${filenames
+        .map((f) => path.relative(process.cwd(), f))
+        .join(" --file ")}`,
+  ],
+};
